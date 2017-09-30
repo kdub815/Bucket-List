f33134cc9e42222b3658c41b00b450df0721548e
Kelsey Huse <kelseyhuse30@gmail.com>
2017-09-29 20:34:55 -0400

    updated script for google place details

--- scripts/google_details.rb
+++ scripts/google_details.rb
@@ -8,18 +8,26 @@ Dotenv.load
 base_uri = 'https://philly-bucketlist.firebaseio.com/'
 firebase = Firebase::Client.new(base_uri)
 
-conn = Faraday.new(:url => 'https://maps.googleapis.com/maps/api/place/textsearch/json')
+places_conn = Faraday.new(:url => 'https://maps.googleapis.com/maps/api/place/textsearch/json')
+details_conn = Faraday.new(:url => 'https://maps.googleapis.com/maps/api/place/details/json')
 
 item_hash = JSON.parse(IO.read('list.json'))
 
 item_hash['items'].each { |item|
 
-	result = conn.get do |req|
+	places_result = places_conn.get do |req|
 		req.params['key'] = ENV['GOOGLE_KEY']
 		req.params['query'] = item['address']	
 	end
 
-	result_json = JSON.parse(result.body)
+	places_json = JSON.parse(places_result.body)
 
-	response = firebase.push("items", { :name => item['name'], :google_data => result_json['results'] })
+	details_result = details_conn.get do |req|
+		req.params['key'] = ENV['GOOGLE_KEY']
+		req.params['placeid'] = places_json['results'][0]['place_id']
+	end
+
+	details_json = JSON.parse(details_result.body)
+
+	response = firebase.push("items", { :name => item['name'], :place_data => places_json['results'], :details_data => details_json['result'] })
 }
